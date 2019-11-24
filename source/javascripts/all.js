d7811d50ad7c70d65b6a18aa058b9fa3e547f75d
hanniabu <hanniabu@gmail.com>
2019-11-23 22:19:58 -0500

    update link target assigner

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,18 +2,18 @@
 //= require ./app/_search
 
 document.addEventListener("DOMContentLoaded", function() {
-	// adds target=_blank to https and pdf links so they open in a new tab
-	var links = document.getElementsByTagName('a');
-	for (var i = 0; i < links.length; i++) {
-		// open external links and pdfs in new tab
-		if (/^(https?:)?\/\/|(\.pdf)/.test(links[i].getAttribute('href'))) {
-			links[i].target = '_blank';
-		}
-		// open docs index in same tab
-		if (/^(https?:\/\/api\.blocknet\.co\/?)$/.test(links[i].getAttribute('href'))) {
-			links[i].target = '_self';
-		}
-	}
+  // adds target=_blank to https and pdf links so they open in a new tab
+  var links = document.getElementsByTagName('a');
+  for (var i = 0; i < links.length; i++) {
+    // open external links and pdfs in new tab
+    if (/(https?)|(\.pdf)|(\.zip)/.test(links[i].getAttribute('href'))) {
+      links[i].target = '_blank';
+    }
+    // if current site and not prf then open in same tab
+    if (/^(?=.*(api\.blocknet\.com))(?!.*(\.pdf))(?!.*(\.zip)).*/.test(links[i].getAttribute('href'))) {
+      links[i].target = '_self';
+    }
+  }
 
 	var notifications = document.getElementById('notification-bar');
 	for (var i = 0; i < notifications.children.length; i++) {
